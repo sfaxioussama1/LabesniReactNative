3d9b9e3a9ec0a9b34711f4610fd75364c610d781
sfaxioussama1 <sf.dev.oussama3@gmail.com>
2020-03-27 13:39:28 +0100

    commit v1.9
    commit v1.9

--- Labesni/screens/HomeScreen.js
+++ Labesni/screens/HomeScreen.js
@@ -2,12 +2,24 @@ import React from "react";
 import { View, Text, ActivityIndicator, StyleSheet,TouchableOpacity } from "react-native";
 import * as firebase from "firebase";
 export default class HomeScreen  extends React.Component {
+    state = { email: "", displayName: "" };
+    componentDidMount() {
+        const { email, displayName } = firebase.auth().currentUser;
 
+        this.setState({ email, displayName });
+    }
+    signOutUser = () => {
+        firebase.auth().signOut();
+    };
 
     render() {
         return (
             <View style={styles.container}>
-                <Text>Home sahbi</Text>
+                <Text>Hi {this.state.email}!</Text>
+
+                <TouchableOpacity style={{ marginTop: 32 }} onPress={this.signOutUser}>
+                    <Text>Logout</Text>
+                </TouchableOpacity>
 
             </View>
         );
